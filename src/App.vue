352f2fd9cba71a8f768ad5178f9133ed67484b00
irwan <irwandasyafriadi96@gmail.com>
2023-11-12 22:12:22 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue'
+import GatauView from "@/views/GatauView.vue";
+import Wawancara from "@/views/Wawancara.vue";
+import HasilView from "@/views/HasilView.vue";
+
+</script>
+
+<template>
+<HomeView/>
+  <div >
+
+  </div>
+  <GatauView/>
+
+  <wawancara/>
+
+  <HasilView/>
+  </template>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+import Navbar from "@/components/Navbar.vue";
+import GatauView from "@/views/GatauView.vue";
+</script>
+
+<template>
+  <Navbar/>
+  <div class="relative" id="home">
+    <div aria-hidden="true" class="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20">
+      <div class="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+      <div class="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+    </div>
+    <Container>
+      <div class="relative pt-36 ml-auto">
+        <div class="lg:w-2/3 text-center mx-auto">
+          <h1 class="text-gray-900 dark:text-white font-bold text-5xl md:text-6xl xl:text-7xl">Pengembangan Destinasi Wisata TMII</h1>
+          <p class="mt-8 text-gray-700 dark:text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio incidunt nam itaque sed eius modi error totam sit illum. Voluptas doloribus asperiores quaerat aperiam. Quidem harum omnis beatae ipsum soluta!</p>
+
+          <div class="hidden py-8 mt-16 border-y border-gray-100 dark:border-gray-800 sm:flex justify-between">
+            <div class="text-left">
+              <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The lowest price</h6>
+              <p class="mt-2 text-gray-500">Some text here</p>
+            </div>
+            <div class="text-left">
+              <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The fastest on the market</h6>
+              <p class="mt-2 text-gray-500">Some text here</p>
+            </div>
+            <div class="text-left">
+              <h6 class="text-lg font-semibold text-gray-700 dark:text-white">The most loved</h6>
+              <p class="mt-2 text-gray-500">Some text here</p>
+            </div>
+          </div>
+        </div>
+        <div class="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
+
+
+
+
+        </div>
+      </div>
+    </Container>
+  </div>
+
+</template>
+
+
+<style scoped>
+
+</style>
